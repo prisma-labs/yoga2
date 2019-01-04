9d4614ad8576b56973d4a4238585866972d86777
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-01-04 15:31:09 +0100

    Split to helpers file

---
+++ packages/yoga/src/helpers.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs'
+import { extname, join } from 'path'
+
+export function findFileByExtension(
+  base: string,
+  ext: string,
+  files?: string[],
+  result?: string[],
+) {
+  files = files || fs.readdirSync(base)
+  result = result || []
+
+  files.forEach(file => {
+    const newbase = join(base, file)
+
+    if (fs.statSync(newbase).isDirectory()) {
+      result = findFileByExtension(
+        newbase,
+        ext,
+        fs.readdirSync(newbase),
+        result,
+      )
+    } else {
+      if (extname(file) === ext) {
+        result!.push(newbase)
+      }
+    }
+  })
+  return result
+}
+
+export function relativeToRootPath(rootPath: string, path: string) {
+  return join(rootPath, path)
+}
