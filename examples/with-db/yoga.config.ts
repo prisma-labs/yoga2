e598b7896343b33631ff340f8db7b6a4dd2369cf
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-01-18 10:58:00 +0100

    Remove config fields and leave defaults

--- examples/with-db/yoga.config.ts
+++ examples/with-db/yoga.config.ts
@@ -1,9 +1,4 @@
 import { InputConfig } from 'yoga'
 
-export default {
-  output: {
-    schemaPath: './src/generated/nexus.graphql',
-    typegenPath: './src/generated/nexus.ts',
-  },
-  prisma: true,
-} as InputConfig
+// Config is empty, defaults are set instead
+export default {} as InputConfig
