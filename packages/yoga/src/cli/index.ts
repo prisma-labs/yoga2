1c3486ffad14074e1db6a5c921264184f9193c05
Wojciech Trocki <wtrocki@redhat.com>
2019-01-25 21:45:41 +0000

    fix: show help when parameters are not empty

--- packages/yoga/src/cli/index.ts
+++ packages/yoga/src/cli/index.ts
@@ -8,21 +8,16 @@ function run() {
   yargs
     .usage('Usage: $0 <command> [options]')
     .command(
-      'dev',
-      'Start the server in dev mode',
-      // @ts-ignore
-      () => {},
-      () => watch(),
+      'dev', 'Start the server in dev mode', {}, watch
     )
     .command(
       'scaffold',
-      'Scaffold a new GraphQL type',
-      () => {},
-      () => scaffold(),
+      'Scaffold a new GraphQL type', {}, scaffold
     )
-    .help('h')
-    .alias('h', 'help')
-    .version().argv
+    .help()
+    .version()
+    .showHelp()
+    .argv
 }
 
 // Only call run when running from CLI, not when included for tests
